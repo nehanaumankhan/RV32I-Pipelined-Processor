23782c9102444080eaa7db6278c06dd0890c9985
nehanaumankhan <nehanaumankhan@gmail.com>
2025-11-13 20:13:13 +0500

    wrapper module and hex_to_7seg implementation

--- RV32I_SCDP/DESIGNS/wrapper.v
+++ RV32I_SCDP/DESIGNS/wrapper.v
@@ -1,41 +1,13 @@
 `include "RISCV_PKG.vh"
 
-// ==================== HEX TO 7-SEGMENT MODULE ====================
-module hex_to_7seg(
-    input [3:0] hex,
-    output reg [6:0] seg // {a,b,c,d,e,f,g}
-);
-    always @(*) begin
-        case(hex)
-            4'h0: seg = 7'b1000000;
-            4'h1: seg = 7'b1111001;
-            4'h2: seg = 7'b0100100;
-            4'h3: seg = 7'b0110000;
-            4'h4: seg = 7'b0011001;
-            4'h5: seg = 7'b0010010;
-            4'h6: seg = 7'b0000010;
-            4'h7: seg = 7'b1111000;
-            4'h8: seg = 7'b0000000;
-            4'h9: seg = 7'b0010000;
-            4'hA: seg = 7'b0001000;
-            4'hB: seg = 7'b0000011;
-            4'hC: seg = 7'b1000110;
-            4'hD: seg = 7'b0100001;
-            4'hE: seg = 7'b0000110;
-            4'hF: seg = 7'b0001110;
-            default: seg = 7'b1111111; // all off
-        endcase
-    end
-endmodule
-
 // ==================== WRAPPER MODULE ====================
 module wrapper (
     input clk100MHz,
     input rst,
     output [6:0] HEX0, HEX1, HEX2, HEX3, // ALU_RESULT lower 16 bits
     output [6:0] HEX4, HEX5, HEX6, HEX7, // DATA_MEMORY_OUT lower 16 bits
-    //output [`INSTRUCTION_SIZE-1:0] alu_result_out,
-    //output [`INSTRUCTION_SIZE-1:0] data_memory_out,
+    output [`INSTRUCTION_SIZE-1:0] alu_result_out,
+    output [`INSTRUCTION_SIZE-1:0] data_memory_out,
     output [`INSTRUCTION_SIZE-1:0] regfile_rd_out,
     output [`INSTRUCTION_SIZE-1:0] pc_out_debug
 );
